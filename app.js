7544bf3d57f6a5b105cab59cb9792993573551c6
Sonia-Baibou <s.baibou@ohmycode.io>
2022-03-25 15:50:06 +0100

    make it better

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ app.use(express.static('./formulaire'));
 app.use('/api/v1/todos', todosRoutes )
 
 
-// app.listen(3300);
+const port = process.env.PORT || 5000;
 
 const start = async()=>{
     try {
         await connectDB(process.env.MONGO_URI)
-        app.listen(3300, ()=> console.log('app listening'))
+        app.listen(port, ()=> console.log('app listening'))
     } catch (error) {
         console.log(error)
     }
