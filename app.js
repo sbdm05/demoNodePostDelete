0fec5f107f277ec13535253645186f5272625c90
Sonia-Baibou <s.baibou@ohmycode.io>
2022-03-25 16:00:16 +0100

    make it better

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static('./formulaire'));
 app.use('/api/v1/todos', todosRoutes )
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 const start = async()=>{
     try {
